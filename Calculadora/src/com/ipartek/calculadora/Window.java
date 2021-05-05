9dd545dd6137c1e600f08726add2559d7b10a9b8
jgarai <83353526+jgarai@users.noreply.github.com>
2021-05-05 13:26:16 +0200

    Reduccion

--- Calculadora/src/com/ipartek/calculadora/Window.java
+++ Calculadora/src/com/ipartek/calculadora/Window.java
@@ -11,6 +11,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Window {
 
@@ -36,8 +38,16 @@ public class Window {
 
 	/**
 	 * Create the application.
+	 * 
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
-	public Window() {
+	public Window() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
 		initialize();
 	}
 
@@ -52,70 +62,65 @@ public class Window {
 		frame.getContentPane().setLayout(null);
 
 		tFnumero1 = new JTextField();
+		tFnumero1.setHorizontalAlignment(SwingConstants.RIGHT);
 		tFnumero1.setText("0");
-		tFnumero1.setBounds(19, 40, 90, 20);
+		tFnumero1.setBounds(19, 40, 75, 20);
 		frame.getContentPane().add(tFnumero1);
 		tFnumero1.setColumns(10);
 
 		tFnumero2 = new JTextField();
+		tFnumero2.setHorizontalAlignment(SwingConstants.RIGHT);
 		tFnumero2.setText("0");
-		tFnumero2.setBounds(169, 40, 90, 20);
+		tFnumero2.setBounds(154, 40, 75, 20);
 		frame.getContentPane().add(tFnumero2);
 		tFnumero2.setColumns(10);
 
 		JComboBox cBoperacion = new JComboBox();
 		cBoperacion.setModel(new DefaultComboBoxModel(new String[] { "+", "-", "*", "/" }));
-		cBoperacion.setBounds(119, 39, 40, 22);
+		cBoperacion.setBounds(104, 39, 40, 22);
 		frame.getContentPane().add(cBoperacion);
 
 		JLabel lblResultado = new JLabel("0");
 		lblResultado.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblResultado.setBounds(364, 43, 60, 14);
+		lblResultado.setBounds(314, 43, 120, 14);
 		frame.getContentPane().add(lblResultado);
 
 		JButton btnCalcular = new JButton("Calcular");
 		btnCalcular.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Double numero1 = Double.parseDouble(tFnumero1.getText());
-				Double numero2 = Double.parseDouble(tFnumero2.getText());
-				Integer operacion = cBoperacion.getSelectedIndex();
-				if (operacion == 0) { // suma
+				try {
+					Double numero1 = Double.parseDouble(tFnumero1.getText());
+					Double numero2 = Double.parseDouble(tFnumero2.getText());
+					Integer operacion = cBoperacion.getSelectedIndex();
 					String result = new String();
-					result += result.format("%s", numero1 + numero2);
-					lblResultado.setText(result);
-					tFnumero1.setText("" + numero1);
-					tFnumero2.setText("" + numero2);
-				}
-				if (operacion == 1) { // resta
-					String result = new String();
-					result += result.format("%s", numero1 - numero2);
-					lblResultado.setText(result);
-					tFnumero1.setText("" + numero1);
-					tFnumero2.setText("" + numero2);
-				}
-				if (operacion == 2) { // multiplicacion
-					String result = new String();
-					result += result.format("%s", numero1 + numero2);
-					lblResultado.setText(result);
-					tFnumero1.setText("" + numero1);
-					tFnumero2.setText("" + numero2);
-				}
-				if (operacion == 3) { // division
-					String result = new String();
-					if (numero2 == 0) {
-						result += "NaN";
-					} else {
-						result += result.format("%s", numero1 / numero2);
+
+					if (operacion == 0) { // suma
+						result = String.valueOf(numero1 + numero2);
+					}
+					if (operacion == 1) { // resta
+						result = String.valueOf(numero1 - numero2);
+					}
+					if (operacion == 2) { // multiplicacion
+						result = String.valueOf(numero1 * numero2);
+					}
+					if (operacion == 3) { // division
+						result = String.valueOf(numero1 / numero2);
 					}
 					lblResultado.setText(result);
-					tFnumero1.setText("" + numero1);
-					tFnumero2.setText("" + numero2);
-				}
+					tFnumero1.setText(String.valueOf(numero1));
+					tFnumero2.setText(String.valueOf(numero2));
 
+				} catch (NumberFormatException e1) {
+					lblResultado.setText("NaN");
+				}
 			}
 		});
-		btnCalcular.setBounds(269, 39, 85, 23);
+		btnCalcular.setBounds(239, 39, 85, 23);
 		frame.getContentPane().add(btnCalcular);
+
+		JLabel lblNewLabel = new JLabel("=");
+		lblNewLabel.setBounds(334, 43, 20, 17);
+		frame.getContentPane().add(lblNewLabel);
 
 	}
 }
